7762760b60de5f0c6a318440050ebb541cd595ff
labprogsam <som3@cin.ufpe.br>
2021-04-26 11:21:42 -0300

    Main: Created base

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # tetris-multimidia
-# tetris-multimidia
---
+++ src/component.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Component() {
+
+  return (
+    <>
+      <button>Add</button>
+    </>
+  );
+}
+
+export default Component;
